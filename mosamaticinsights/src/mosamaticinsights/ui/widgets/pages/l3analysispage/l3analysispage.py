660796a77a585e4f8cb52675dd1baa221dca312d
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2026-02-20 13:37:24 +0100

    Saving work

--- mosamaticinsights/src/mosamaticinsights/ui/widgets/pages/l3analysispage/l3analysispage.py
+++ mosamaticinsights/src/mosamaticinsights/ui/widgets/pages/l3analysispage/l3analysispage.py
@@ -5,6 +5,8 @@ from PySide6.QtWidgets import (
     QPushButton,
     QVBoxLayout,
     QHBoxLayout,
+    QLineEdit,
+    QFileDialog,
     QStyle,
 )
 from PySide6.QtGui import QFont
@@ -17,6 +19,11 @@ class L3AnalysisPage(Page):
     def __init__(self, name, title, settings):
         super(L3AnalysisPage, self).__init__(name, title, settings)
         self._home_button = None
+        self._load_model_line_edit = None
+        self._load_model_button = None
+        self._load_images_line_edit = None
+        self._load_images_button = None
+        self._run_button = None
         self.init()
 
     # INITIALIZATION
@@ -25,9 +32,23 @@ class L3AnalysisPage(Page):
         home_button_layout = QHBoxLayout()
         home_button_layout.addWidget(self.home_button())
         home_button_layout.addWidget(QLabel('Go to home'))
+        load_model_layout = QHBoxLayout()
+        load_model_layout.addWidget(self.load_model_line_edit())
+        load_model_layout.addWidget(self.load_model_button())
+        load_images_layout = QHBoxLayout()
+        load_images_layout.addWidget(self.load_images_line_edit())
+        load_images_layout.addWidget(self.load_images_button())
+        run_layout = QHBoxLayout()
+        run_layout.addWidget(self.run_button())
+        run_layout.addWidget(QLabel('Run analysis'))
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignmentFlag.AlignTop)
         layout.addLayout(home_button_layout)
+        layout.addWidget(QLabel('Load segmentation AI model'))
+        layout.addLayout(load_model_layout)
+        layout.addWidget(QLabel('Load images'))
+        layout.addLayout(load_images_layout)
+        layout.addLayout(run_layout)
         self.setLayout(layout)
 
     # GETTERS
@@ -40,6 +61,34 @@ class L3AnalysisPage(Page):
             self._home_button.setIcon(QApplication.style().standardIcon(QStyle.StandardPixmap.SP_ArrowBack))
             self._home_button.clicked.connect(self.handle_home_button)
         return self._home_button
+    
+    def load_model_line_edit(self):
+        if not self._load_model_line_edit:
+            self._load_model_line_edit = QLineEdit()
+        return self._load_model_line_edit
+    
+    def load_model_button(self):
+        if not self._load_model_button:
+            self._load_model_button = QPushButton('Select directory...')
+        return self._load_model_button
+    
+    def load_images_line_edit(self):
+        if not self._load_images_line_edit:
+            self._load_images_line_edit = QLineEdit()
+        return self._load_images_line_edit
+    
+    def load_images_button(self):
+        if not self._load_images_button:
+            self._load_images_button = QPushButton('Select directory...')
+        return self._load_images_button
+    
+    def run_button(self):
+        if not self._run_button:
+            self._run_button = QPushButton()
+            self._run_button.setFlat(True)
+            self._run_button.setFixedWidth(BUTTON_WIDTH)
+            self._run_button.setIcon(QApplication.style().standardIcon(QStyle.StandardPixmap.SP_ArrowForward))
+        return self._run_button
 
     # EVENT HANDLERS
 
